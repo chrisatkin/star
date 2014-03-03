62f156aca3fabc8e1e67e0f4a258418a3a9edab7
Chris Atkin <me@chrisatk.in>
2014-03-03 10:12:34 -0800

    Add to Git

---
+++ Rhapsody/SkeletalPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace org.btg.Star.Rhapsody
+{
+    public struct SkeletalPoint
+    {
+        public int x
+        {
+            get;
+            set;
+        }
+
+        public int y
+        {
+            get;
+            set;
+        }
+
+        public int z
+        {
+            get;
+            set;
+        }
+
+        public SkeletalPointType type
+        {
+            get;
+            set;
+        }
+    }
+}
